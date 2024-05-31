4b50f5ad4793d9390f3f22d9370ac22bf160c9ca
Facundo Enriquez <fenriquez@inym.org.ar>
2024-05-31 12:58:35 -0300

    done rest framework simple with crud in react

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,6 +1,64 @@
+import { useForm } from 'react-hook-form'
+import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.api'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+
 const TaskFormPage = () => {
+
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm()
+    const navigate = useNavigate()
+    const params = useParams()
+
+    const onSubmit = handleSubmit(async data => {
+        if (params.id) {
+            await updateTask(data, params.id)
+        } else {
+            await createTask(data)
+        }
+        navigate('/')
+    })
+
+    useEffect(() => {
+        async function loadTask() {
+            if (params.id) {
+                const { data } = await getTask(params.id)
+                setValue('title', data.title)
+                setValue('description', data.description)
+            }
+        }
+        loadTask()
+    }, [])
+
+
+
     return (
-        <div>TaskFormPage</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="title"
+                    {...register("title", { required: true })}
+                />
+                {errors.title && <span>this field is required</span>}
+                <textarea
+                    rows={"3"}
+                    placeholder="Description"
+                    {...register("description", { required: true })}
+                ></textarea>
+                {errors.description && <span>this field is required</span>}
+                <button>Save</button>
+            </form>
+
+            {
+                params.id && <button onClick={async () => {
+                    const accepted = window.confirm('estas seguro?')
+                    if (accepted) {
+                        await deleteTask(params.id)
+                        navigate('/tasks')
+                    }
+                }}>Delete</button>
+            }
+        </div>
     )
 }
 export default TaskFormPage
--- tasks/models.py
+++ tasks/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=50),
+    title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
